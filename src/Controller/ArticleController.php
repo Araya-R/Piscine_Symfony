5d278d81266a0090e47b00f83ac17a839d706bba
Araya RAKSACHOB <araya.raksachob@lapiscine.pro>
2025-04-22 16:29:47 +0200

    affichageArticles

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,22 +2,44 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ArticleController{
+class ArticleController extends AbstractController{
 
     #[Route("/list-articles", name:"listArticles")]
     public function listArticles (){
-        var_dump('Page liste des articles');die;
+
+        $articles = [
+            1 => [
+                "id" => 1,
+                "title" => "Article 1",
+                "content" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur tenetur repellendus quod voluptates nemo amet voluptas pariatur, molestias magnam ab cumque quas deserunt, voluptate, nobis iste quae dolores? Debitis, sunt.",
+                "created_at" => new \DateTime(),
+                "image" => "https://cdn.pixabay.com/photo/2018/08/04/11/30/draw-3583548_1280.png",
+                "is_published" => true
+            ],
+
+            2 => [
+                "id" => 2,
+                "title" => "Article 2",
+                "content" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur tenetur repellendus quod voluptates nemo amet voluptas pariatur, molestias magnam ab cumque quas deserunt, voluptate, nobis iste quae dolores? Debitis, sunt.",
+                "created_at" => new \DateTime(),
+                "image" => "https://cdn.pixabay.com/photo/2018/08/04/11/30/draw-3583548_1280.png",
+                "is_published" => false
+            ],
+            3 => [
+                "id" => 3,
+                "title" => "Article 3",
+                "content" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur tenetur repellendus quod voluptates nemo amet voluptas pariatur, molestias magnam ab cumque quas deserunt, voluptate, nobis iste quae dolores? Debitis, sunt.",
+                "created_at" => new \DateTime(),
+                "image" => "https://cdn.pixabay.com/photo/2018/08/04/11/30/draw-3583548_1280.png",
+                "is_published" => true
+            ]
+        ];
+
+        return $this->render('listArticles.html.twig', ['articles'=>$articles]);
     }
 
-    #[Route("/create-article",name:"createArticle")]
-    public function createArticle (){
-        var_dump('Page création article');die;
-    }
-
-    #[Route("/delete-article", name:"daleteArticle")]
-    public function deleteArticle (){
-        var_dump('Page delete article');die;
-    }
+    
 }
--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -4,29 +4,31 @@
 namespace App\Controller;
 //remplace le require
 
-//=namespace de la classe Response 
-use Symfony\Component\HttpFoundation\Response;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 //c'est le namespace (chemin) de la classe Route qu'on utilise au dessus des méthodes
 use Symfony\Component\Routing\Annotation\Route;
 
-//Je crée une classe HomeController
-class PageController{
+//Je crée une classe HomeController qui gère une/+ routes
+//je fais hériter la classe de AbstractController afin de lui donner accès 
+//à render()=> pour afficher une vue Twig
+class PageController extends AbstractController{
 
     //Je déclare une route dont "/" correspond à la racine du site 
     //et on donne un nom à cette route "home"
     #[Route("/", name:"home")]
     //Je crée une méthode qui me retourne un var_dump
     public function home(){
-        return new Response("Bienvenue sur la page d'accueil!", 200);
+        return $this->render('home.html.twig');
     }
 
     #[Route ("/propos",name:"about")]
     public function about(){
-       return new Response("ABOUT US", 200);
+       return $this->render('about.html.twig');
     }
 
     #[Route("/contact", name:"contact")]
     public function contact(){
-        return new Response("CONTACT US", 200);
+        return $this->render('contact.html.twig');
     }
 }
