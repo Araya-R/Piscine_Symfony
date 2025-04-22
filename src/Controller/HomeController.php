96c3366ed369ecd668fb2dbee538c4786cebea24
Araya RAKSACHOB <araya.raksachob@lapiscine.pro>
2025-04-22 12:07:27 +0200

    Symfony

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+//Je crée une classe HomeController
+class HomeController{
+
+    //Je déclare une route dont "/" correspond à la racine du site 
+    //et on donne un nom à cette route "home"
+    #[Route('/', name:"home")]
+
+    //Je crée une méthode qui me retourne un var_dump
+    public function home(){
+        var_dump ('Page accueil');die;
+    }
+}
