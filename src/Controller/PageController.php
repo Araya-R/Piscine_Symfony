951db1339981a2566890a5eccbec4d05bc087d7e
Araya RAKSACHOB <araya.raksachob@lapiscine.pro>
2025-04-22 13:55:00 +0200

    InstallationApache

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -4,14 +4,23 @@ namespace App\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 
 //Je crée une classe HomeController
-class HomeController{
+class PageController{
 
     //Je déclare une route dont "/" correspond à la racine du site 
     //et on donne un nom à cette route "home"
-    #[Route('/', name:"home")]
-
+    #[Route("/", name:"home")]
     //Je crée une méthode qui me retourne un var_dump
     public function home(){
         var_dump ('Page accueil');die;
+    }
+
+    #[Route ("/propos",name:"about")]
+    public function about(){
+        var_dump('Page à Propos');die;
+    }
+
+    #[Route("/contact", name:"contact")]
+    public function contact(){
+        var_dump('Page de contact');die;
     }
 }
